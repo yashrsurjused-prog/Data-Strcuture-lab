8f785ddc6dfe47506bc7b97ff85375d963292d6d
Yash Surjuse <yashrsurjused@gmail.com>
2026-03-27 14:31:40 +0530

    Add initial structure for DFS implementation

---
+++ 9.1.1. Depth-First Search (DFS).c
@@ -0,0 +1,30 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VERTICES 100
+
+// Graph represented using adjacency list
+typedef struct Node {
+    int vertex;
+    struct Node* next;
+} Node;
+
+Node* adjList[MAX_VERTICES];
+int visited[MAX_VERTICES];
+
+// Function to create a new adjacency list node
+Node* createNode(int vertex) {
+    //write your code here...
+    
+    
+    
+    
+}
+
+// Function to add an edge to the graph
+void addEdge(int u, int v) {
+    //write your code here...
+    
+    
+    
+}
